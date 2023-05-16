65d42fcfc494519fcf3d2acd7469cbdef1adcbd0
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-05-16 11:04:27 -0400

    update

--- mygroovy.groovy
+++ mygroovy.groovy
@@ -15,30 +15,32 @@ versions:ver1
 data=[[name: "comp1",type: "type1"],[name: "comp2", type: "type2"]]
 version=[ "name:xyz", "type:daemon", "versions:qqq", "type:type1", "versions:ver1", "type:type2","version:vers2"]
 println "Hello"
-def flag=false
-def start=true
-type=''
-def lss=[]
-def map=[:]
-rawdata.eachLine {
-    kv=it.tokenize(":")
-    if (it.contains('name')){
-        if ( !start ) {
-           println "assign map"
-           lss.add(map); map=[:]
-           map[kv[0]]=kv[1]
-        } else { start=false }
-        map[kv[0]]=kv[1]
-    } else if (it.contains('daemon')){ 
-        flag=true; return true
-    } else if ( flag && it.contains('type')){ 
-        type=kv[1]; return true
-    } else if (type != ''){
-        lss.each { cmp->
-            if (cmp['type']==type){
-                cmp['version']=kv[1];type = ''
+def parseSolution(String data){
+    def flag=false
+    def start=true
+    type=''
+    def lss=[]
+    def map=[:]
+    rawdata.eachLine {
+        kv=it.tokenize(":")
+        if (it.contains('name')){
+            if ( !start ) {
+            lss.add(map); map=[:]
+            map[kv[0]]=kv[1]
+            } else { start=false }
+            map[kv[0]]=kv[1]
+        } else if (it.contains('daemon')){ 
+            flag=true; return true
+        } else if ( flag && it.contains('type')){ 
+            type=kv[1]; return true
+        } else if (type != ''){
+            lss.each { cmp->
+                if (cmp['type']==type){
+                    cmp['version']=kv[1];type = ''
+                }
             }
-        }
-    } else { map[kv[0]]=kv[1]}
+        } else { map[kv[0]]=kv[1]}
+    }
+    return lss
 }
-println "lss=$lss"
+println parseSolution(rawdata)
