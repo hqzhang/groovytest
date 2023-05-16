eb622f0424fddd0124c7eeb69e635613b5efebd7
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-05-16 09:32:55 -0400

    update

--- mygroovy.groovy
+++ mygroovy.groovy
@@ -1,15 +1,53 @@
-import groovy.json.JsonOutput;
-import groovy.yaml.YamlSlurper
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 
+rawdata="""\
+"name:comp1"
+"type:type1"
+"name:comp2"
+"type:type2"
+"name:xyz"
+"type:daemon"
+"versions:qqq"
+"type:type1" 
+"versions:ver1"
+ "type:type2"
+ "version:vers2"
+ """
+data=[[name: "comp1",type: "type1"],[name: "comp2", type: "type2"]]
+version=[ "name:xyz", "type:daemon", "versions:qqq", "type:type1", "versions:ver1", "type:type2","version:vers2"]
+println "Hello"
+def flag=false
+def start=true
+type=''
+def lss=[]
+def map=[:]
+rawdata.eachLine {
+    kv=it.tokenize(":")
+    if (it.contains('name')){
+        map[kv[0]]=kv[1]
+        println "start=$start"
+        if ( !start ) {
+           println "assign map"
+           lss.add(map); map=[:]
+           map[kv[0]]=kv[1]
+        } else { start=false }
+    } else {
+    if (it.contains('daemon')){ 
+       flag=true
+       return true
+    } else if ( flag && it.contains('type')){ 
+        type=kv[1]
+       return true
+    } else if (type != ''){
+        
 
-println("enter main")	
-String fileConts = new File('config').text
-println fileConts
-def yamlSlurper = new YamlSlurper().parseText(fileConts)
-println  yamlSlurper
-def mapper = new ObjectMapper().writeValueAsString(yamlSlurper)
-println "output json..."
-println JsonOutput.prettyPrint(mapper)
-
+        lss.each { cmp->
+            println " loop ${cmp}"
+            println "  loop ${cmp['type']}"
+            if ( cmp['type']==type ){
+                cmp['version']=kv[1];type = ''
+            }
+        }
+    } else { map[kv[0]]=kv[1]}
+    }
+}
+println "lss=$lss"
